d622fb0a080bad30dd173cd4fccd630a88a9bb17
moozer <bigmoozer@gmail.com>
2020-12-10 21:16:56 +0100

    fix key and crt locations in scripts

--- templates/create_client_keys.sh
+++ templates/create_client_keys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 client=$1
 
 if [ x$client = x ]; then
@@ -7,27 +9,27 @@ if [ x$client = x ]; then
     exit 1
 fi
 
-if [ ! -e keys/$client.key ]; then
+if [ ! -e pki/private/$client.key ]; then
     echo "Generating keys..."
-    . vars
-    ./pkitool $client
+    ./easyrsa gen-req $client nopass
+    ./easyrsa sign-req client $client
     echo "...keys generated."
 fi
 
-tarball=./keys/$client.tgz
+tarball=./pki/$client.tgz
 
 if [ ! -e $tarball ]; then
     echo "Creating tarball..."
     tmpdir=/tmp/client-tar.$$
     mkdir $tmpdir
     cp {{ openvpn_client_conf }} $tmpdir/{{ openvpn_client_conf }}
-    cp keys/ca.crt $tmpdir 
-    cp keys/ta.key $tmpdir 
-    cp keys/$client.key $tmpdir/client.key
-    cp keys/$client.crt $tmpdir/client.crt
+    cp pki/ca.crt $tmpdir 
+    cp pki/private/ta.key $tmpdir 
+    cp pki/private/$client.key $tmpdir/client.key
+    cp pki/issued/$client.crt $tmpdir/client.crt
     tar -C $tmpdir -czvf $tarball .
     rm -rf $tmpdir
     echo "...tarball created" 
 else
-    echo "Nothing to do, so nothing done. (keys/$client.tgz already exists)" 
+    echo "Nothing to do, so nothing done. ($tarball already exists)" 
 fi
